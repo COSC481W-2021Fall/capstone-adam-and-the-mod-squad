76d05c70b3148276aead4c00cba28980bbea2bcf
sschafe <sschafe2@emich.edu>
2021-10-10 14:06:44 -0400

    Revert "Revert "Merge branch 'sara-user-log-in-ui' into sara-home-page-base-template""
    This reverts commit 6421018cd163fba97dd7b4782ba346d40ff903cf.

--- mydjangoproject/mydjangoproject/urls.py
+++ mydjangoproject/mydjangoproject/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('muninn.urls')),
+    path('users/', include('users.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('admin/', admin.site.urls),
 ]
