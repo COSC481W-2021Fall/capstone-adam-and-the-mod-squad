bc1bbf7558bc0f0e9d675afb84a9827f5b711dec
AndreakaJump <72317368+AndreakaJump@users.noreply.github.com>
2021-10-11 16:38:18 -0400

    impl: add task
    complete form to add task
    issue #12

--- mydjangoproject/muninn/views.py
+++ mydjangoproject/muninn/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Task
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from datetime import date
 
 def home(request):
     return render(request, 'muninn/home.html')
@@ -12,7 +13,10 @@ def home(request):
 class dashboard(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
-
+    def post(self, request, *args, **kwargs):
+        form = Task(title=request.POST.get('task'), created=date.today(), user=request.user)
+        form.save()
+        return redirect('muninn-dashboard')
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
