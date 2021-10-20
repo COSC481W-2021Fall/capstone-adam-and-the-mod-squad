4b4a27516776119b24e95c505d4697ef3b48bc00
apollac4 <72466147+apollac4@users.noreply.github.com>
2021-10-20 19:06:03 -0400

    impl: added Animals model into Admin

--- mydjangoproject/muninn/admin.py
+++ mydjangoproject/muninn/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Task
+from .models import Task, Animals
 # from .models import MuninnUserv2 (EXAMPLE)
 # admin.site.register(MuninnUserv2)
 
 # ^ follow above logic to add tables to admin page
 
 admin.site.register(Task)
+admin.site.register(Animals)
